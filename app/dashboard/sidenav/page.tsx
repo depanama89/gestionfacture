7f92419e37122fe8be74693f70a38297517fd6bd
depanama <kudiatutomanitu@gmail.com>
2025-07-30 16:26:24 +0100

    update login component ,dashboard

--- app/dashboard/sidenav/page.tsx
+++ app/dashboard/sidenav/page.tsx
@@ -10,7 +10,7 @@ export default function Sidebar() {
     <aside className=" flex h-full flex-col px-4 py-4 md:px-6">
       <Link
         href="/"
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-700 p-4 md:h-40"
+        className="mb-2 flex h-20 items-end justify-start rounded-md bg-[#333] p-4 md:h-40"
       >
         <div className="w-32 text-white md:w-40">
           <AcmeLogo />
