2da01d076bf86b9397987f94c25176dc95cd4677
depanama <kudiatutomanitu@gmail.com>
2025-07-31 11:32:20 +0100

    update all files

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,4 +1,4 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { CurrencyDollarIcon, GlobeAltIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts'
 
 export default function AcmeLogo() {
@@ -6,7 +6,8 @@ export default function AcmeLogo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+      <CurrencyDollarIcon className="h-12 w-12 "/>
+      {/* <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" /> */}
       <p className="text-[40px]">Invoxia</p>
     </div>
   );
